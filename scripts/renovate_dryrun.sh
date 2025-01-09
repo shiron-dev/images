05d385d519c4e4565da2722ac31d29c0a9ac4b93
shiron <shiron4710.dev@gmail.com>
2025-01-09 11:25:24 +0900

    chore: fix renovate dryrun script

--- scripts/renovate_dryrun.sh
+++ scripts/renovate_dryrun.sh
@@ -4,8 +4,17 @@ set -e
 
 renovate-config-validator --strict
 
-export RENOVATE_CONFIG_FILE=.github/renovate.json
+RENOVATE_CONFIG_FILE=.github/renovate.json
 
-export RENOVATE_TOKEN="${RENOVATE_TOKEN:-$(read -p "環境変数 'RENOVATE_TOKEN' が設定されていません。値を入力してください: " input_value; echo "$input_value")}"
+RENOVATE_BASE_BRANCHES="$(git branch --show-current)"
 
-LOG_LEVEL=debug renovate shiron-dev/images --dry-run=full --require-config=ignored
+# export RENOVATE_TOKEN="${RENOVATE_TOKEN:-$(read -p -r "環境変数 'RENOVATE_TOKEN' が設定されていません。値を入力してください: " input_value; echo "$input_value")}"
+
+RENOVATE_BASE_BRANCHES="$RENOVATE_BASE_BRANCHES" \
+ RENOVATE_CONFIG_FILE="$RENOVATE_CONFIG_FILE" \
+ LOG_LEVEL=debug \
+ renovate \
+ --platform=local \
+ --dry-run=full --require-config=ignored \
+ --schedule="" --onboarding=false \
+#  shiron-dev/images
